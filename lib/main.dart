b29c83fc72425c487c2917a6d8d17d8bbe3ed2ca
DeniJuliSetiawan112 <denijuli.setiawan@gmail.com>
2022-04-23 23:17:32 +0700

    make splash screen page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shamo_app/config/theme.dart';
+import 'package:shamo_app/pages/sign_in_page.dart';
+import 'package:shamo_app/pages/splash_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,22 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Text(
-        'Hallo Semua',
-        style: priceTextStyle.copyWith(fontSize: 50),
-      )),
+      routes: {
+        '/': (context) => SplashPage(),
+        '/sign-in': (context) => SignInPage()
+      },
     );
   }
 }
