01d3d983c2e90d7b11b3cc66b1f3c053291ba88e
DeniJuliSetiawan112 <denijuli.setiawan@gmail.com>
2022-05-15 21:08:55 +0700

    profile page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -4,15 +4,129 @@ import 'package:shamo_app/config/theme.dart';
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Profile Page',
-          style: primaryTextStyle,
+  Widget header() {
+    return AppBar(
+      backgroundColor: bgColor1,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      flexibleSpace: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(30),
+          child: Row(
+            children: [
+              ClipOval(
+                child: Image.asset(
+                  'assets/images/profile.png',
+                  width: 64,
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hallo, Alex',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      '@alexkeinn',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Image.asset(
+                'assets/icons/ic_logout.png',
+                width: 20,
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget menuItem(String text) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 16,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: secondaryTextStyle.copyWith(
+              fontSize: 13,
+            ),
+          ),
+          Icon(
+            Icons.chevron_right,
+            color: primaryTextColor,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget content() {
+    return Expanded(
+      child: Container(
+        color: bgColor3,
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Account',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            menuItem('Edit Profile'),
+            menuItem('Your Orders'),
+            menuItem('Help'),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'General',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            menuItem('Privacy & Policy'),
+            menuItem('Term of Service'),
+            menuItem('Rate App'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
